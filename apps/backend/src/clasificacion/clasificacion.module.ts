631492e911f22ce06cfbb4f184508b727c6c8e05
Ana Ruth Rivera <anaruth.riverasalgado@gmail.com>
2026-09-21 08:53:20 -0600

    feat: integrate ESP32 connection and HTTP service for triggering actions in ESP32

--- apps/backend/src/clasificacion/clasificacion.module.ts
+++ apps/backend/src/clasificacion/clasificacion.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { ClasificacionController } from './clasificacion.controller';
 import { ClasificacionService } from './clasificacion.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { QrModule } from '../qr/qr.module';
 
 @Module({
-  imports: [PrismaModule, QrModule],
+  imports: [PrismaModule, QrModule, HttpModule.register({ timeout: 15000 }),],
   controllers: [ClasificacionController],
   providers: [ClasificacionService],
   exports: [ClasificacionService],
